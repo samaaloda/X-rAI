5b14b896d113daa1d41d44b0a7f0896dc0ceb420
Sama <samaaloda27@gmail.com>
2025-06-14 23:01:58 -0400

    added tflite instead

--- backend/fracture.py
+++ backend/fracture.py
@@ -1,48 +1,34 @@
 import os
 import shutil
 import cv2
-import requests
-from fastapi.responses import JSONResponse
 import numpy as np
 import tensorflow as tf
-from fastapi import FastAPI, File, UploadFile, HTTPException
+import boto3
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 import uvicorn
-import io
 
-
-import boto3
 bucket_name = 'x-rai-store'
-model_key = 'fracture_classes.h5'
-local_path = 'model.h5'
+model_key = 'fracture_model.tflite'
+local_path = 'model.tflite'
 
+s3 = boto3.client('s3', region_name='us-east-2')
+s3.download_file(bucket_name, model_key, local_path)
 
-s3= boto3.client('s3', region_name='us-east-2') #initialize client
-s3.download_file(bucket_name, model_key, local_path) #download file to local_path
-
-
-model = tf.keras.models.load_model(local_path) #load it
-
-
-
+tflite_interpreter = tf.lite.Interpreter(model_path=local_path)
+tflite_interpreter.allocate_tensors()
+input_details = tflite_interpreter.get_input_details()
+output_details = tflite_interpreter.get_output_details()
 
 class_names = [
-    'Avulsion fracture',
-    'Comminuted fracture',
-    'Fracture Dislocation',
-    'Greenstick fracture',
-    'Hairline Fracture',
-    'Impacted fracture',
-    'Longitudinal fracture',
-    'Oblique fracture',
-    'Pathological fracture',
-    'Spiral Fracture'
+    'Avulsion fracture', 'Comminuted fracture', 'Fracture Dislocation',
+    'Greenstick fracture', 'Hairline Fracture', 'Impacted fracture',
+    'Longitudinal fracture', 'Oblique fracture', 'Pathological fracture', 'Spiral Fracture'
 ]
 
-
 app = FastAPI()
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -52,15 +38,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 if not os.path.exists('./uploads'):
     os.makedirs('./uploads')
-
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the fracture prediction API"}
-
 
 @app.post("/upload/")
 async def upload_images(images: List[UploadFile] = File(...)):
@@ -71,10 +54,8 @@ async def upload_images(images: List[UploadFile] = File(...)):
                 status_code=400
             )
 
-
         clear_image_path = None
         max_laplacian_var = 0
-
 
         for image in images:
             file_path = f"./uploads/{image.filename}"
@@ -82,14 +63,10 @@ async def upload_images(images: List[UploadFile] = File(...)):
                 contents = await image.read()
                 buffer.write(contents)
 
-
             laplacian_var = calculate_laplacian_variance(file_path)
-
-
             if laplacian_var > max_laplacian_var:
                 max_laplacian_var = laplacian_var
                 clear_image_path = file_path
-
 
         if clear_image_path is None:
             return JSONResponse(
@@ -97,9 +74,8 @@ async def upload_images(images: List[UploadFile] = File(...)):
                 status_code=400
             )
 
-
         processed_img = preprocess_image(clear_image_path)
-        predicted_class, confidence = predict_image(processed_img, model, class_names)
+        predicted_class, confidence = predict_image(processed_img)
         return {
             "message": "File uploaded and processed successfully",
             "clearest_image": clear_image_path.split("/")[-1],
@@ -109,7 +85,6 @@ async def upload_images(images: List[UploadFile] = File(...)):
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
-
 def preprocess_image(img_path):
     img = cv2.imread(img_path)
     if img is None:
@@ -117,16 +92,16 @@ def preprocess_image(img_path):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.resize(img, (256, 256))
     img = img / 255.0
-    img_array = np.expand_dims(img, axis=0)
+    img_array = np.expand_dims(img, axis=0).astype(np.float32)
     return img_array
 
-
-def predict_image(processed_img, model, class_names):
-    prediction = model.predict(processed_img)
+def predict_image(processed_img):
+    tflite_interpreter.set_tensor(input_details[0]['index'], processed_img)
+    tflite_interpreter.invoke()
+    prediction = tflite_interpreter.get_tensor(output_details[0]['index'])
     predicted_class = class_names[np.argmax(prediction)]
     confidence = np.max(prediction)
     return predicted_class, confidence
-
 
 def calculate_laplacian_variance(img_path):
     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
@@ -134,7 +109,5 @@ def calculate_laplacian_variance(img_path):
         raise ValueError(f"Unable to read the image file at {img_path}.")
     return cv2.Laplacian(img, cv2.CV_64F).var()
 
-
 if __name__ == "__main__":
     uvicorn.run("fracture:app", host="0.0.0.0", port=5000, reload=True)
-
